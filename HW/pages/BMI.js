07782e26620cda6ad5257e2e5926c7deff03fdae
DonnaNedeleva <92685885+DonnaNedeleva@users.noreply.github.com>
2022-01-27 18:52:00 +0200

    Успешно направени някои задачи.
    -Arrayasobject
    -BMI-Изчисляване на индекс на телесна маса
    -CalculateRectangle(asobject)Area
    -incrementandcomparisonoperators
    -Simplevariablesandoperations
    -StudentsDataAsArrays
    -variableshadowingTask

---
+++ HW/pages/BMI.js
@@ -0,0 +1,12 @@
+//ИЗЧИСЛЯВАНЕ НА BMI -  ИНДЕКС НА ТЕЛЕСНАТА МАСА
+
+var high=3;
+console.log ("high:"+ high);
+
+var teglo=54;
+
+console.log ("teglo:"+ teglo);
+
+var bmi = teglo/high**2;
+
+console.log ("bmi:"+ bmi);
---
+++ HW/pages/CalculateRectangle(as object)Area.js
@@ -0,0 +1,32 @@
+//TASK: 
+//да се дефинира функция calcRectangleArea(), която връща лицето на правоъгълник, със зададени дължина и ширина:
+
+// ---> YOUR CODE HERE <---
+
+let rectWidth = 6;
+let rectHeight = 3;
+let area = calcRectangleArea(rectWidth, rectHeight);
+function calcRectangleArea(){
+    
+    let area = rectWidth* rectHeight;
+    console.log(`The area of rectangle is: ${area}`);//18
+    
+    
+}
+
+//}
+//console.log(`The area of rectangle is: ${area}`);
+
+// !!! Do not change code bellow
+// TEST:
+//let rectWidth = 6;
+//let rectHeight = 3;
+
+//let area = calcRectangleArea(rectWidth, rectHeight);
+
+//console.log(`The area of rectangle is: ${area}`);
+
+// EXPECTED RESULT:
+// The area of rectangle is: 18
+
+
